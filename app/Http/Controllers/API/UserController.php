e69f3c6ca188b0b878c5946534f96c95a57fe74d
Roel Wilhelm <roelwilhelm@gmail.com>
2018-10-14 19:52:03 +0800

    User Profile Component

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -53,6 +53,11 @@ class UserController extends Controller
         //
     }
 
+
+    public function profile(){
+        return auth('api')->user();
+    }
+
     /**
      * Update the specified resource in storage.
      *
